0b1055dd4fd69c63dc4ec938538d51efedb4d67c
Afzal Ansari <dev.afzalansari@gmail.com>
2023-02-05 13:35:00 +0545

    add types for escaped style props

--- src/utils/extract-props.ts
+++ src/utils/extract-props.ts
@@ -1,4 +1,7 @@
-export function extractProps({ className = '', style = {}, ...props }: Record<string, any>, sprinkles: any) {
+export function extractProps(
+  { className = '', style = {}, ...props }: Record<string, any>,
+  sprinkles: any
+) {
   let sprinkleProps: Record<string, any> = {}
   let escapedStyles: Record<string, any> = {}
   let otherProps: Record<string, any> = {}
@@ -7,7 +10,7 @@ export function extractProps({ className = '', style = {}, ...props }: Record<st
     if (sprinkles.properties.has(key)) {
       sprinkleProps[key] = props[key]
     } else if (key.startsWith('__')) {
-      escapedStyles[(key.substring(2))] = props[key]
+      escapedStyles[key.substring(2)] = props[key]
     } else {
       otherProps[key] = props[key]
     }
