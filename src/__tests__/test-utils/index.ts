53b6fb9d5724c99e4425c59ab5f9271ef546a794
Afzal Ansari <dev.afzalansari@gmail.com>
2023-02-12 11:02:12 +0545

    add tests

--- src/__tests__/test-utils/index.ts
+++
@@ -1,4 +0,0 @@
-import { createFactory } from "../..";
-import { sprinkles } from "./sprinkles.css";
-
-export const s = createFactory({ sprinkles })
---
+++ src/__tests__/test-utils/index.tsx
@@ -0,0 +1,24 @@
+import * as React from 'react'
+import type { ReactNode } from 'react'
+import { createFactory } from '../..'
+import { sprinkles } from './sprinkles.css'
+
+type CustomElementProps = {
+  element: string
+  classes: string
+  props: { children: ReactNode; type: 'submit' }
+}
+
+export const s = createFactory({
+  sprinkles,
+  customElement: ({ element, classes, props }: CustomElementProps) => {
+    if (element === 'Button') {
+      return (
+        <button className={classes} type={props.type}>
+          {props.children}
+        </button>
+      )
+    }
+    return null
+  }
+})
